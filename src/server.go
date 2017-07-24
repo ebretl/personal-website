3bced3f284d22544af8028c6c2ca2af2e3c496cb
Evan Bretl <evanbretl4@gmail.com>
2017-07-25 00:48:49 +0200

    serves my most listened tracks

--- src/server.go
+++ src/server.go
@@ -4,14 +4,38 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "strings"
+    "os/exec"
+    "time"
 )
 
+
+var top_songs []string
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    bytes, _ := ioutil.ReadFile("top_songs.txt")
-    fmt.Fprintf(w, string(bytes))
+    fmt.Fprintf(w, "%v", top_songs)
+}
+
+func refresh_top_songs() {
+    exec.Command("python3", "songs/get_top_songs.py").Run()
+    bytes, _ := ioutil.ReadFile("songs/top_songs.txt")
+    top_songs = strings.Split(string(bytes), "\n")
+    top_songs = top_songs[:len(top_songs)-1]
+    fmt.Println("refreshed top songs list")
+}
+
+func song_refresher_thread() {
+    for {
+        refresh_top_songs()
+        time.Sleep(time.Minute * 10)
+    }
 }
 
 func main() {
+    go song_refresher_thread()
+
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":80", nil)
+    port := ":8080"
+    fmt.Println("serving", port)
+    http.ListenAndServe(port, nil)
 }
--- src/songs/get_top_songs.py
+++ src/songs/get_top_songs.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 url = 'https://www.last.fm/user/evanxq/library/tracks?date_preset=LAST_90_DAYS'
 r = requests.get(url).text
 soup = BeautifulSoup(r, 'html.parser')
 
-with open('top_songs.txt','w',encoding='utf-8') as f:
+fname = os.path.join(os.path.dirname(__file__), 'top_songs.txt')
+with open(fname,'w',encoding='utf-8') as f:
     for item in soup.find_all('td', 'chartlist-name'):
         song = item.find('a', 'link-block-target').attrs['title']
         f.write(song.format('utf-8') + '\n')
